0b2792a31b3ef8c3c74e1dcea763848aa487385c
arikamel <arian.kamel7@gmail.com>
2025-04-15 19:44:13 -0400

    Fix geolocation permissions policy to allow location access

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    res.setHeader('Permissions-Policy', 'geolocation=()');
+    res.setHeader('Permissions-Policy', 'geolocation=(self)');
     next();
 });
 
